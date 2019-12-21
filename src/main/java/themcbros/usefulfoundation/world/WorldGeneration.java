6f4e238d51b326258d91f812e7ea3e0289aa6a76
TheMCBrothers <themcbrothers2016@gmail.com>
2019-12-21 12:40:45 +0100

    Added ore generation for the new materials

--- src/main/java/themcbros/usefulfoundation/world/WorldGeneration.java
+++ src/main/java/themcbros/usefulfoundation/world/WorldGeneration.java
@@ -19,9 +19,17 @@ public class WorldGeneration {
 
             final BlockState copperOre = FoundationBlocks.COPPER_ORE.getDefaultState();
             final BlockState tinOre = FoundationBlocks.TIN_ORE.getDefaultState();
+            final BlockState nickelOre = FoundationBlocks.NICKEL_ORE.getDefaultState();
+            final BlockState leadOre = FoundationBlocks.LEAD_ORE.getDefaultState();
+            final BlockState silverOre = FoundationBlocks.SILVER_ORE.getDefaultState();
+            final BlockState uraniumOre = FoundationBlocks.URANIUM_ORE.getDefaultState();
 
-            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, copperOre, 8)).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(15, 40, 0, 90))));
-            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, tinOre, 8)).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(15, 20, 0, 80))));
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, copperOre, 8)).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(10, 40, 0, 90))));
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, tinOre, 8)).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(10, 20, 0, 80))));
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, nickelOre, 4)).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(8, 0, 0, 24))));
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, leadOre, 5)).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(9, 0, 0, 30))));
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, silverOre, 5)).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(9, 0, 0, 30))));
+            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.func_225566_b_(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, uraniumOre, 3)).func_227228_a_(Placement.COUNT_RANGE.func_227446_a_(new CountRangeConfig(5, 0, 0, 18))));
 
 
         }
