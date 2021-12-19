56c639257ff44f63cf8adc2349214692f936e043
TheMCCrazyMan <themccrazyman@mail.ch>
2021-12-19 20:35:04 +0100

    added plate recipes, cooking recipes and raw material recipes

--- src/main/java/themcbros/usefulfoundation/data/FoundationRecipeProvider.java
+++ src/main/java/themcbros/usefulfoundation/data/FoundationRecipeProvider.java
@@ -1,11 +1,9 @@
 package themcbros.usefulfoundation.data;
 
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.Tags;
 
 import javax.annotation.Nonnull;
@@ -42,6 +40,48 @@ public class FoundationRecipeProvider extends RecipeProvider {
         ShapelessRecipeBuilder.shapeless(TIN_INGOT, 9).requires(BLOCKS_TIN).unlockedBy("has_tin_block", has(BLOCKS_TIN)).save(recipe, getId("tin_ingot_from_tin_block"));
         ShapelessRecipeBuilder.shapeless(URANIUM_INGOT, 9).requires(BLOCKS_URANIUM).unlockedBy("has_uranium_block", has(BLOCKS_URANIUM)).save(recipe, getId("uranium_ingot_from_uranium_block"));
 
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(RAW_MATERIALS_BRONZE), BRONZE_INGOT, 0.7F, 200).unlockedBy("has_raw_bronze", has(RAW_MATERIALS_BRONZE)).save(recipe, getId("bronze_ingot_from_smelting_raw_bronze"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(RAW_MATERIALS_BRONZE), BRONZE_INGOT, 0.7F, 100).unlockedBy("has_raw_bronze", has(RAW_MATERIALS_BRONZE)).save(recipe, getId("bronze_ingot_from_blasting_raw_bronze"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(RAW_MATERIALS_ELECTRUM), ELECTRUM_INGOT, 0.7F, 200).unlockedBy("has_raw_electrum", has(RAW_MATERIALS_ELECTRUM)).save(recipe, getId("electrum_ingot_from_smelting_raw_electrum"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(RAW_MATERIALS_ELECTRUM), ELECTRUM_INGOT, 0.7F, 100).unlockedBy("has_raw_electrum", has(RAW_MATERIALS_ELECTRUM)).save(recipe, getId("electrum_ingot_from_blasting_raw_electrum"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(RAW_MATERIALS_INVAR), INVAR_INGOT, 0.7F, 200).unlockedBy("has_raw_invar", has(RAW_MATERIALS_INVAR)).save(recipe, getId("invar_ingot_from_smelting_raw_invar"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(RAW_MATERIALS_INVAR), INVAR_INGOT, 0.7F, 100).unlockedBy("has_raw_invar", has(RAW_MATERIALS_INVAR)).save(recipe, getId("invar_ingot_from_blasting_raw_invar"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(RAW_MATERIALS_LEAD), LEAD_INGOT, 0.7F, 200).unlockedBy("has_raw_lead", has(RAW_MATERIALS_LEAD)).save(recipe, getId("lead_ingot_from_smelting_raw_lead"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(RAW_MATERIALS_LEAD), LEAD_INGOT, 0.7F, 100).unlockedBy("has_raw_lead", has(RAW_MATERIALS_LEAD)).save(recipe, getId("lead_ingot_from_blasting_raw_lead"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(RAW_MATERIALS_NICKEL), NICKEL_INGOT, 0.7F, 200).unlockedBy("has_raw_nickel", has(RAW_MATERIALS_NICKEL)).save(recipe, getId("nickel_ingot_from_smelting_raw_nickel"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(RAW_MATERIALS_NICKEL), NICKEL_INGOT, 0.7F, 100).unlockedBy("has_raw_nickel", has(RAW_MATERIALS_NICKEL)).save(recipe, getId("nickel_ingot_from_blasting_raw_nickel"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(RAW_MATERIALS_SILVER), SILVER_INGOT, 0.7F, 200).unlockedBy("has_raw_silver", has(RAW_MATERIALS_SILVER)).save(recipe, getId("silver_ingot_from_smelting_raw_silver"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(RAW_MATERIALS_SILVER), SILVER_INGOT, 0.7F, 100).unlockedBy("has_raw_silver", has(RAW_MATERIALS_SILVER)).save(recipe, getId("silver_ingot_from_blasting_raw_silver"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(RAW_MATERIALS_TIN), TIN_INGOT, 0.7F, 200).unlockedBy("has_raw_tin", has(RAW_MATERIALS_TIN)).save(recipe, getId("tin_ingot_from_smelting_raw_tin"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(RAW_MATERIALS_TIN), TIN_INGOT, 0.7F, 100).unlockedBy("has_raw_tin", has(RAW_MATERIALS_TIN)).save(recipe, getId("tin_ingot_from_blasting_raw_tin"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(RAW_MATERIALS_URANIUM), URANIUM_INGOT, 0.7F, 200).unlockedBy("has_raw_uranium", has(RAW_MATERIALS_URANIUM)).save(recipe, getId("uranium_ingot_from_smelting_raw_uranium"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(RAW_MATERIALS_URANIUM), URANIUM_INGOT, 0.7F, 100).unlockedBy("has_raw_uranium", has(RAW_MATERIALS_URANIUM)).save(recipe, getId("uranium_ingot_from_blasting_raw_uranium"));
+
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(ORES_LEAD), LEAD_INGOT, 0.7F, 200).unlockedBy("has_lead_ore", has(ORES_LEAD)).save(recipe, getId("lead_ingot_from_smelting_lead_ore"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(ORES_LEAD), LEAD_INGOT, 0.7F, 100).unlockedBy("has_lead_ore", has(ORES_LEAD)).save(recipe, getId("lead_ingot_from_blasting_lead_ore"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(ORES_NICKEL), NICKEL_INGOT, 0.7F, 200).unlockedBy("has_nickel_ore", has(ORES_NICKEL)).save(recipe, getId("nickel_ingot_from_smelting_nickel_ore"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(ORES_NICKEL), NICKEL_INGOT, 0.7F, 100).unlockedBy("has_nickel_ore", has(ORES_NICKEL)).save(recipe, getId("nickel_ingot_from_blasting_nickel_ore"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(ORES_SILVER), SILVER_INGOT, 0.7F, 200).unlockedBy("has_silver_ore", has(ORES_SILVER)).save(recipe, getId("silver_ingot_from_smelting_silver_ore"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(ORES_SILVER), SILVER_INGOT, 0.7F, 100).unlockedBy("has_silver_ore", has(ORES_SILVER)).save(recipe, getId("silver_ingot_from_blasting_silver_ore"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(ORES_TIN), TIN_INGOT, 0.7F, 200).unlockedBy("has_tin_ore", has(ORES_TIN)).save(recipe, getId("tin_ingot_from_smelting_tin_ore"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(ORES_TIN), TIN_INGOT, 0.7F, 100).unlockedBy("has_tin_ore", has(ORES_TIN)).save(recipe, getId("tin_ingot_from_blasting_tin_ore"));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(ORES_URANIUM), URANIUM_INGOT, 0.7F, 200).unlockedBy("has_uranium_ore", has(ORES_URANIUM)).save(recipe, getId("uranium_ingot_from_smelting_uranium_ore"));
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(ORES_URANIUM), URANIUM_INGOT, 0.7F, 100).unlockedBy("has_uranium_ore", has(ORES_URANIUM)).save(recipe, getId("uranium_ingot_from_blasting_uranium_ore"));
+
+        //Raw Materials
+        ShapelessRecipeBuilder.shapeless(RAW_BRONZE, 4).requires(RAW_MATERIALS_COPPER).requires(RAW_MATERIALS_COPPER).requires(RAW_MATERIALS_COPPER).requires(RAW_MATERIALS_TIN).unlockedBy("has_raw_copper", has(RAW_MATERIALS_COPPER)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(RAW_ELECTRUM, 2).requires(RAW_MATERIALS_GOLD).requires(RAW_MATERIALS_SILVER).unlockedBy("has_raw_silver", has(RAW_MATERIALS_SILVER)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(RAW_INVAR, 3).requires(RAW_MATERIALS_IRON).requires(RAW_MATERIALS_IRON).requires(RAW_MATERIALS_NICKEL).unlockedBy("has_raw_iron", has(RAW_MATERIALS_IRON)).save(recipe);
+
+        ShapelessRecipeBuilder.shapeless(RAW_BRONZE, 9).requires(RAW_BLOCKS_BRONZE).unlockedBy("has_raw_bronze", has(RAW_BLOCKS_BRONZE)).save(recipe, getId("raw_bronze_from_raw_bronze_block"));
+        ShapelessRecipeBuilder.shapeless(RAW_ELECTRUM, 9).requires(RAW_BLOCKS_ELECTRUM).unlockedBy("has_raw_electrum", has(RAW_BLOCKS_ELECTRUM)).save(recipe, getId("raw_electrum_from_raw_electrum_block"));
+        ShapelessRecipeBuilder.shapeless(RAW_INVAR, 9).requires(RAW_BLOCKS_INVAR).unlockedBy("has_raw_invar", has(RAW_BLOCKS_INVAR)).save(recipe, getId("raw_invar_from_raw_invar_block"));
+        ShapelessRecipeBuilder.shapeless(RAW_LEAD, 9).requires(RAW_BLOCKS_LEAD).unlockedBy("has_raw_lead", has(RAW_BLOCKS_LEAD)).save(recipe, getId("raw_lead_from_raw_lead_block"));
+        ShapelessRecipeBuilder.shapeless(RAW_NICKEL, 9).requires(RAW_BLOCKS_NICKEL).unlockedBy("has_raw_nickel", has(RAW_BLOCKS_NICKEL)).save(recipe, getId("raw_nickel_from_raw_nickel_block"));
+        ShapelessRecipeBuilder.shapeless(RAW_SILVER, 9).requires(RAW_BLOCKS_SILVER).unlockedBy("has_raw_silver", has(RAW_BLOCKS_SILVER)).save(recipe, getId("raw_silver_from_raw_silver_block"));
+        ShapelessRecipeBuilder.shapeless(RAW_TIN, 9).requires(RAW_BLOCKS_TIN).unlockedBy("has_raw_tin", has(RAW_BLOCKS_TIN)).save(recipe, getId("raw_tin_from_raw_tin_block"));
+        ShapelessRecipeBuilder.shapeless(RAW_URANIUM, 9).requires(RAW_BLOCKS_URANIUM).unlockedBy("has_raw_uranium", has(RAW_BLOCKS_URANIUM)).save(recipe, getId("raw_uranium_from_raw_uranium_block"));
+
         //Gears
         ShapedRecipeBuilder.shaped(BRONZE_GEAR).pattern(" X ").pattern("X X").pattern(" X ").define('X', INGOTS_BRONZE).unlockedBy("has_bronze_ingot", has(INGOTS_BRONZE)).save(recipe);
         ShapedRecipeBuilder.shaped(COPPER_GEAR).pattern(" X ").pattern("X X").pattern(" X ").define('X', INGOTS_COPPER).unlockedBy("has_copper_ingot", has(INGOTS_COPPER)).save(recipe);
@@ -55,6 +95,19 @@ public class FoundationRecipeProvider extends RecipeProvider {
         ShapedRecipeBuilder.shaped(TIN_GEAR).pattern(" X ").pattern("X X").pattern(" X ").define('X', INGOTS_TIN).unlockedBy("has_tin_ingot", has(INGOTS_TIN)).save(recipe);
         ShapedRecipeBuilder.shaped(URANIUM_GEAR).pattern(" X ").pattern("X X").pattern(" X ").define('X', INGOTS_URANIUM).unlockedBy("has_uranium_ingot", has(INGOTS_URANIUM)).save(recipe);
 
+        //Plates
+        ShapelessRecipeBuilder.shapeless(BRONZE_PLATE).requires(INGOTS_BRONZE).requires(HAMMER).unlockedBy("has_bronze_ingot", has(INGOTS_BRONZE)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(COPPER_PLATE).requires(INGOTS_COPPER).requires(HAMMER).unlockedBy("has_copper_ingot", has(INGOTS_COPPER)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(ELECTRUM_PLATE).requires(INGOTS_ELECTRUM).requires(HAMMER).unlockedBy("has_electrum_ingot", has(INGOTS_ELECTRUM)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(GOLD_PLATE).requires(Tags.Items.INGOTS_GOLD).requires(HAMMER).unlockedBy("has_gold_ingot", has(Tags.Items.INGOTS_GOLD)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(INVAR_PLATE).requires(INGOTS_INVAR).requires(HAMMER).unlockedBy("has_invar_ingot", has(INGOTS_INVAR)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(IRON_PLATE).requires(Tags.Items.INGOTS_IRON).requires(HAMMER).unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(LEAD_PLATE).requires(INGOTS_LEAD).requires(HAMMER).unlockedBy("has_lead_ingot", has(INGOTS_LEAD)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(NICKEL_PLATE).requires(INGOTS_NICKEL).requires(HAMMER).unlockedBy("has_nickel_ingot", has(INGOTS_NICKEL)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(SILVER_PLATE).requires(INGOTS_SILVER).requires(HAMMER).unlockedBy("has_silver_ingot", has(INGOTS_SILVER)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(TIN_PLATE).requires(INGOTS_TIN).requires(HAMMER).unlockedBy("has_tin_ingot", has(INGOTS_TIN)).save(recipe);
+        ShapelessRecipeBuilder.shapeless(URANIUM_PLATE).requires(INGOTS_URANIUM).requires(HAMMER).unlockedBy("has_uranium_ingot", has(INGOTS_URANIUM)).save(recipe);
+
         //Nuggets
         ShapelessRecipeBuilder.shapeless(BRONZE_NUGGET, 9).requires(INGOTS_BRONZE).unlockedBy("has_bronze_ingot", has(INGOTS_BRONZE)).save(recipe);
         ShapelessRecipeBuilder.shapeless(COPPER_NUGGET, 9).requires(INGOTS_COPPER).unlockedBy("has_copper_ingot", has(INGOTS_COPPER)).save(recipe);
@@ -66,7 +119,7 @@ public class FoundationRecipeProvider extends RecipeProvider {
         ShapelessRecipeBuilder.shapeless(TIN_NUGGET, 9).requires(INGOTS_TIN).unlockedBy("has_tin_ingot", has(INGOTS_TIN)).save(recipe);
         ShapelessRecipeBuilder.shapeless(URANIUM_NUGGET, 9).requires(INGOTS_URANIUM).unlockedBy("has_uranium_ingot", has(INGOTS_URANIUM)).save(recipe);
 
-        //Blocks
+        //Storage Blocks
         ShapedRecipeBuilder.shaped(BRONZE_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', INGOTS_BRONZE).unlockedBy("has_bronze_ingot", has(INGOTS_BRONZE)).save(recipe);
         ShapedRecipeBuilder.shaped(Items.COPPER_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', INGOTS_COPPER).unlockedBy("has_copper_ingot", has(INGOTS_COPPER)).save(recipe);
         ShapedRecipeBuilder.shaped(ELECTRUM_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', INGOTS_ELECTRUM).unlockedBy("has_electrum_ingot", has(INGOTS_ELECTRUM)).save(recipe);
@@ -76,6 +129,16 @@ public class FoundationRecipeProvider extends RecipeProvider {
         ShapedRecipeBuilder.shaped(SILVER_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', INGOTS_SILVER).unlockedBy("has_silver_ingot", has(INGOTS_SILVER)).save(recipe);
         ShapedRecipeBuilder.shaped(TIN_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', INGOTS_TIN).unlockedBy("has_tin_ingot", has(INGOTS_TIN)).save(recipe);
         ShapedRecipeBuilder.shaped(URANIUM_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', INGOTS_URANIUM).unlockedBy("has_uranium_ingot", has(INGOTS_URANIUM)).save(recipe);
+
+        //Raw Storage Blocks
+        ShapedRecipeBuilder.shaped(RAW_BRONZE_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', RAW_BRONZE).unlockedBy("has_raw_bronze", has(RAW_BRONZE)).save(recipe);
+        ShapedRecipeBuilder.shaped(RAW_ELECTRUM_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', RAW_ELECTRUM).unlockedBy("has_raw_electrum", has(RAW_ELECTRUM)).save(recipe);
+        ShapedRecipeBuilder.shaped(RAW_INVAR_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', RAW_INVAR).unlockedBy("has_raw_invar", has(RAW_INVAR)).save(recipe);
+        ShapedRecipeBuilder.shaped(RAW_LEAD_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', RAW_LEAD).unlockedBy("has_raw_lead", has(RAW_LEAD)).save(recipe);
+        ShapedRecipeBuilder.shaped(RAW_NICKEL_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', RAW_NICKEL).unlockedBy("has_raw_nickel", has(RAW_NICKEL)).save(recipe);
+        ShapedRecipeBuilder.shaped(RAW_SILVER_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', RAW_SILVER).unlockedBy("has_raw_silver", has(RAW_SILVER)).save(recipe);
+        ShapedRecipeBuilder.shaped(RAW_TIN_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', RAW_TIN).unlockedBy("has_raw_tin", has(RAW_TIN)).save(recipe);
+        ShapedRecipeBuilder.shaped(RAW_URANIUM_BLOCK).pattern("XXX").pattern("XXX").pattern("XXX").define('X', RAW_URANIUM).unlockedBy("has_raw_uranium", has(RAW_URANIUM)).save(recipe);
 
         //Tools
         ShapedRecipeBuilder.shaped(HAMMER).pattern(" X#").pattern(" RX").pattern("R  ").define('X', Tags.Items.INGOTS_IRON).define('#', Tags.Items.STRING).define('R', Tags.Items.RODS_WOODEN).unlockedBy("has_iron_ingot", has(Items.IRON_INGOT)).save(recipe);
--- src/main/java/themcbros/usefulfoundation/items/HammerItem.java
+++ src/main/java/themcbros/usefulfoundation/items/HammerItem.java
@@ -1,16 +1,23 @@
 package themcbros.usefulfoundation.items;
 
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.Tags;
 
+import java.util.Random;
+
 public class HammerItem extends DiggerItem {
-    public HammerItem(Properties builder) {
-        super(0, -2.5f, Tiers.IRON, BlockTags.MINEABLE_WITH_SHOVEL, builder);
+    public static final Random RAND = new Random();
+
+    public HammerItem(Properties props) {
+        super(0, -2.5f, Tiers.IRON, BlockTags.MINEABLE_WITH_SHOVEL, props);
     }
 
     @Override
@@ -21,6 +28,21 @@ public class HammerItem extends DiggerItem {
     @Override
     public boolean isCorrectToolForDrops(BlockState state) {
         return state.is(Tags.Blocks.COBBLESTONE) || state.is(Tags.Blocks.SAND) || state.is(Tags.Blocks.GRAVEL) || state.is(Tags.Blocks.ORES);
+    }
+
+    @Override
+    public boolean hasContainerItem(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getContainerItem(ItemStack stack) {
+        ItemStack copy = stack.copy();
+
+        if(copy.hurt(1, RAND, null))
+            return ItemStack.EMPTY;
+        else
+            return copy;
     }
 
     public float getDestroySpeed(ItemStack stack, BlockState state) {
