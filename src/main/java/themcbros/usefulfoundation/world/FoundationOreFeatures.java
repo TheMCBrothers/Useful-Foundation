5ecbb6ea44c6cef76f54021eeaf94e6df465a5e0
TheMCCrazyMan <themccrazyman@mail.ch>
2022-08-09 20:37:07 +0200

    bump version to 6.0.0

--- src/main/java/themcbros/usefulfoundation/world/FoundationOreFeatures.java
+++ src/main/java/themcbros/usefulfoundation/world/FoundationOreFeatures.java
@@ -1,5 +1,6 @@
 package themcbros.usefulfoundation.world;
 
+import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -22,11 +23,11 @@ public final class FoundationOreFeatures {
     public static final List<OreConfiguration.TargetBlockState> ORE_URANIUM_TARGET_LIST = List.of(OreConfiguration.target(STONE_ORE_REPLACEABLES, FoundationBlocks.URANIUM_ORE.defaultBlockState()), OreConfiguration.target(DEEPSLATE_ORE_REPLACEABLES, FoundationBlocks.DEEPSLATE_URANIUM_ORE.defaultBlockState()));
 
     //Features
-    public static final ConfiguredFeature<?, ?> ORE_ALUMINUM = FeatureUtils.register("ore_aluminum", Feature.ORE.configured(new OreConfiguration(ORE_ALUMINUM_TARGET_LIST, 8)));
-    public static final ConfiguredFeature<?, ?> ORE_LEAD = FeatureUtils.register("ore_lead", Feature.ORE.configured(new OreConfiguration(ORE_LEAD_TARGET_LIST, 8)));
-    public static final ConfiguredFeature<?, ?> ORE_NICKEL = FeatureUtils.register("ore_nickel", Feature.ORE.configured(new OreConfiguration(ORE_NICKEL_TARGET_LIST, 8)));
-    public static final ConfiguredFeature<?, ?> ORE_PLATINUM = FeatureUtils.register("ore_platinum", Feature.ORE.configured(new OreConfiguration(ORE_PLATINUM_TARGET_LIST, 4)));
-    public static final ConfiguredFeature<?, ?> ORE_SILVER = FeatureUtils.register("ore_silver", Feature.ORE.configured(new OreConfiguration(ORE_SILVER_TARGET_LIST, 8)));
-    public static final ConfiguredFeature<?, ?> ORE_TIN = FeatureUtils.register("ore_tin", Feature.ORE.configured(new OreConfiguration(ORE_TIN_TARGET_LIST, 8)));
-    public static final ConfiguredFeature<?, ?> ORE_URANIUM = FeatureUtils.register("ore_uranium", Feature.ORE.configured(new OreConfiguration(ORE_URANIUM_TARGET_LIST, 8)));
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_ALUMINUM = FeatureUtils.register("ore_aluminum", Feature.ORE, new OreConfiguration(ORE_ALUMINUM_TARGET_LIST, 8));
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_LEAD = FeatureUtils.register("ore_lead", Feature.ORE, new OreConfiguration(ORE_LEAD_TARGET_LIST, 8));
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_NICKEL = FeatureUtils.register("ore_nickel", Feature.ORE, new OreConfiguration(ORE_NICKEL_TARGET_LIST, 8));
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_PLATINUM = FeatureUtils.register("ore_platinum", Feature.ORE, new OreConfiguration(ORE_PLATINUM_TARGET_LIST, 4));
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_SILVER = FeatureUtils.register("ore_silver", Feature.ORE, new OreConfiguration(ORE_SILVER_TARGET_LIST, 8));
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_TIN = FeatureUtils.register("ore_tin", Feature.ORE, new OreConfiguration(ORE_TIN_TARGET_LIST, 8));
+    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_URANIUM = FeatureUtils.register("ore_uranium", Feature.ORE, new OreConfiguration(ORE_URANIUM_TARGET_LIST, 8));
 }
