fea41bdd6d6d8521687ff90e5918c2f63b1d6dbd
TheMCLoveMan <themcloveman@gmail.com>
2024-02-20 22:47:17 +0100

    use library deferred registers

--- src/main/java/net/themcbrothers/usefulfoundation/core/FoundationBlocks.java
+++ src/main/java/net/themcbrothers/usefulfoundation/core/FoundationBlocks.java
@@ -7,58 +7,45 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.themcbrothers.usefulfoundation.block.UsefulBeehiveBlock;
 
-import java.util.function.Function;
-
 import static net.themcbrothers.usefulfoundation.core.Registration.BLOCKS;
-import static net.themcbrothers.usefulfoundation.core.Registration.ITEMS;
 
 public final class FoundationBlocks {
     static void init() {
     }
 
-    public static final DeferredBlock<BeehiveBlock> USEFUL_BEEHIVE = registerBlock("useful_beehive", UsefulBeehiveBlock::new, BlockBehaviour.Properties.of().sound(SoundType.WOOD));
-    public static final DeferredBlock<Block> TIN_ORE = registerBlock("tin_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE));
-    public static final DeferredBlock<Block> DEEPSLATE_TIN_ORE = registerBlock("deepslate_tin_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> TIN_BLOCK = registerBlock("tin_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> RAW_TIN_BLOCK = registerBlock("raw_tin_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> BRONZE_BLOCK = registerBlock("bronze_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> NICKEL_ORE = registerBlock("nickel_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> DEEPSLATE_NICKEL_ORE = registerBlock("deepslate_nickel_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> NICKEL_BLOCK = registerBlock("nickel_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> RAW_NICKEL_BLOCK = registerBlock("raw_nickel_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> INVAR_BLOCK = registerBlock("invar_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> LEAD_ORE = registerBlock("lead_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> DEEPSLATE_LEAD_ORE = registerBlock("deepslate_lead_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> LEAD_BLOCK = registerBlock("lead_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> RAW_LEAD_BLOCK = registerBlock("raw_lead_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> SILVER_ORE = registerBlock("silver_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> DEEPSLATE_SILVER_ORE = registerBlock("deepslate_silver_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> SILVER_BLOCK = registerBlock("silver_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> RAW_SILVER_BLOCK = registerBlock("raw_silver_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> ELECTRUM_BLOCK = registerBlock("electrum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> URANIUM_ORE = registerBlock("uranium_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> DEEPSLATE_URANIUM_ORE = registerBlock("deepslate_uranium_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> URANIUM_BLOCK = registerBlock("uranium_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> RAW_URANIUM_BLOCK = registerBlock("raw_uranium_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> ALUMINUM_ORE = registerBlock("aluminum_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> DEEPSLATE_ALUMINUM_ORE = registerBlock("deepslate_aluminum_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> ALUMINUM_BLOCK = registerBlock("aluminum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> RAW_ALUMINUM_BLOCK = registerBlock("raw_aluminum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> PLATINUM_ORE = registerBlock("platinum_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> DEEPSLATE_PLATINUM_ORE = registerBlock("deepslate_platinum_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> PLATINUM_BLOCK = registerBlock("platinum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> RAW_PLATINUM_BLOCK = registerBlock("raw_platinum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> ENDERIUM_BLOCK = registerBlock("enderium_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> SIGNALUM_BLOCK = registerBlock("signalum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-    public static final DeferredBlock<Block> STEEL_BLOCK = registerBlock("steel_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
-
-    private static DeferredBlock<Block> registerBlock(String name, BlockBehaviour.Properties props) {
-        return registerBlock(name, Block::new, props);
-    }
-
-    private static <B extends Block> DeferredBlock<B> registerBlock(String name, Function<BlockBehaviour.Properties, ? extends B> func, BlockBehaviour.Properties props) {
-        DeferredBlock<B> deferredBlock = BLOCKS.registerBlock(name, func, props);
-        ITEMS.registerSimpleBlockItem(deferredBlock);
-        return deferredBlock;
-    }
+    public static final DeferredBlock<BeehiveBlock> USEFUL_BEEHIVE = BLOCKS.registerBlock("useful_beehive", UsefulBeehiveBlock::new, BlockBehaviour.Properties.of().sound(SoundType.WOOD));
+    public static final DeferredBlock<Block> TIN_ORE = BLOCKS.registerSimpleBlock("tin_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE));
+    public static final DeferredBlock<Block> DEEPSLATE_TIN_ORE = BLOCKS.registerSimpleBlock("deepslate_tin_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> TIN_BLOCK = BLOCKS.registerSimpleBlock("tin_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> RAW_TIN_BLOCK = BLOCKS.registerSimpleBlock("raw_tin_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> BRONZE_BLOCK = BLOCKS.registerSimpleBlock("bronze_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> NICKEL_ORE = BLOCKS.registerSimpleBlock("nickel_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> DEEPSLATE_NICKEL_ORE = BLOCKS.registerSimpleBlock("deepslate_nickel_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> NICKEL_BLOCK = BLOCKS.registerSimpleBlock("nickel_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> RAW_NICKEL_BLOCK = BLOCKS.registerSimpleBlock("raw_nickel_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> INVAR_BLOCK = BLOCKS.registerSimpleBlock("invar_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> LEAD_ORE = BLOCKS.registerSimpleBlock("lead_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> DEEPSLATE_LEAD_ORE = BLOCKS.registerSimpleBlock("deepslate_lead_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> LEAD_BLOCK = BLOCKS.registerSimpleBlock("lead_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> RAW_LEAD_BLOCK = BLOCKS.registerSimpleBlock("raw_lead_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> SILVER_ORE = BLOCKS.registerSimpleBlock("silver_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> DEEPSLATE_SILVER_ORE = BLOCKS.registerSimpleBlock("deepslate_silver_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> SILVER_BLOCK = BLOCKS.registerSimpleBlock("silver_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> RAW_SILVER_BLOCK = BLOCKS.registerSimpleBlock("raw_silver_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> ELECTRUM_BLOCK = BLOCKS.registerSimpleBlock("electrum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> URANIUM_ORE = BLOCKS.registerSimpleBlock("uranium_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> DEEPSLATE_URANIUM_ORE = BLOCKS.registerSimpleBlock("deepslate_uranium_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> URANIUM_BLOCK = BLOCKS.registerSimpleBlock("uranium_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> RAW_URANIUM_BLOCK = BLOCKS.registerSimpleBlock("raw_uranium_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> ALUMINUM_ORE = BLOCKS.registerSimpleBlock("aluminum_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> DEEPSLATE_ALUMINUM_ORE = BLOCKS.registerSimpleBlock("deepslate_aluminum_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> ALUMINUM_BLOCK = BLOCKS.registerSimpleBlock("aluminum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> RAW_ALUMINUM_BLOCK = BLOCKS.registerSimpleBlock("raw_aluminum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> PLATINUM_ORE = BLOCKS.registerSimpleBlock("platinum_ore", BlockBehaviour.Properties.of().strength(3f).sound(SoundType.STONE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> DEEPSLATE_PLATINUM_ORE = BLOCKS.registerSimpleBlock("deepslate_platinum_ore", BlockBehaviour.Properties.of().strength(4.5F, 3F).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> PLATINUM_BLOCK = BLOCKS.registerSimpleBlock("platinum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> RAW_PLATINUM_BLOCK = BLOCKS.registerSimpleBlock("raw_platinum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> ENDERIUM_BLOCK = BLOCKS.registerSimpleBlock("enderium_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> SIGNALUM_BLOCK = BLOCKS.registerSimpleBlock("signalum_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
+    public static final DeferredBlock<Block> STEEL_BLOCK = BLOCKS.registerSimpleBlock("steel_block", BlockBehaviour.Properties.of().strength(5f).sound(SoundType.METAL).requiresCorrectToolForDrops());
 }
--- src/main/java/net/themcbrothers/usefulfoundation/core/Registration.java
+++ src/main/java/net/themcbrothers/usefulfoundation/core/Registration.java
@@ -5,24 +5,26 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.themcbrothers.lib.registries.BlockDeferredRegister;
+import net.themcbrothers.lib.registries.ItemDeferredRegister;
 import net.themcbrothers.usefulfoundation.UsefulFoundation;
 
 public final class Registration {
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(UsefulFoundation.MOD_ID);
+    public static final ItemDeferredRegister ITEMS = ItemDeferredRegister.create(UsefulFoundation.MOD_ID);
+    public static final BlockDeferredRegister BLOCKS = BlockDeferredRegister.create(UsefulFoundation.MOD_ID, ITEMS);
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, UsefulFoundation.MOD_ID);
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(UsefulFoundation.MOD_ID);
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, UsefulFoundation.MOD_ID);
 
 
     public static void register(IEventBus modEventBus) {
+        FoundationItems.init();
         FoundationBlocks.init();
         FoundationBlockEntityTypes.init();
-        FoundationItems.init();
         FoundationTabs.init();
 
+        ITEMS.register(modEventBus);
         BLOCKS.register(modEventBus);
         BLOCK_ENTITY_TYPES.register(modEventBus);
-        ITEMS.register(modEventBus);
         CREATIVE_MODE_TABS.register(modEventBus);
     }
 }
